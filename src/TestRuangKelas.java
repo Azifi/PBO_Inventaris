2a90ca1841189e5107df794cbca594dd5d0fce57
DANIEMB <DANIEMB@EMB-PC>
2015-04-29 14:07:08 +0700

    ini kelasnya

---
+++ src/TestRuangKelas.java
@@ -0,0 +1,10 @@
+public class TestRuangKelas {
+	public static void main(String[] auch) {
+		ProsesRkelas digin = new ProsesRkelas();
+		digin.IdentifiRkelas();
+		digin.KondisiRkelas();
+		digin.Output();
+		digin.Tampil();
+
+	}
+}
